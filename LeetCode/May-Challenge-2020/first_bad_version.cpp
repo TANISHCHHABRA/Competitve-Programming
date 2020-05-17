d8d8e1398c9a6b7d722ef0aad7a0b2c410a251bd
TANISH <52378401+TANISHCHHABRA@users.noreply.github.com>
2020-05-17 23:28:55 +0530

    Day 1 Solution

--- LeetCode/May-Challenge-2020/first_bad_version.cpp
+++ LeetCode/May-Challenge-2020/first_bad_version.cpp
@@ -1,20 +1,21 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution {
-public:
-    int firstBadVersion(int n) 
-    {
-        int start = 1;
-        int end = n;
-        while(start < end)
+class Solution 
+{
+    public:
+        int firstBadVersion(int n) 
         {
-            int mid = start + (end - start) / 2;
-            if(isBadVersion(mid))
-                end = mid;
-            else
-                start = mid + 1;
+            int start = 1;
+            int end = n;
+            while(start < end)
+            {
+                int mid = start + (end - start) / 2;
+                if(isBadVersion(mid))
+                    end = mid;
+                else
+                    start = mid + 1;
+            }
+            return start;
         }
-        return start;
-    }
 };
